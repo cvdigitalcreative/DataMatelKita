8c6294dd19b7f1bc798e0e669dacfd81aadbdfbf
Malianzikri <malian@bahasakita.co.id>
2019-09-06 20:25:16 +0700

    fixing optimasi

--- app/src/main/java/com/digitalcreative/aplikasidatamining/View/MenuPages/PencarianPage_Activity.java
+++ app/src/main/java/com/digitalcreative/aplikasidatamining/View/MenuPages/PencarianPage_Activity.java
@@ -551,7 +551,7 @@ public class PencarianPage_Activity extends AppCompatActivity {
             // completely detached from realm and is not monitored by realm
             // for changes. Thus this list of values is free to move around
             // inside any thread.
-            list= realm.where(Model_LacakMobil.class).beginsWith("no_plat",search.getText().toString(), Case.INSENSITIVE).or().beginsWith("noka",search.getText().toString(), Case.INSENSITIVE).or().beginsWith("nosin",search.getText().toString(), Case.INSENSITIVE).findAll().sort("no_plat");
+            list= realm.where(Model_LacakMobil.class).beginsWith("no_plat",search.getText().toString(), Case.INSENSITIVE).or().beginsWith("noka",search.getText().toString(), Case.INSENSITIVE).or().beginsWith("nosin",search.getText().toString(), Case.INSENSITIVE).findAll();
             List<Model_LacakMobil> safeWords;
             System.out.println("search done");
             if(list.size()>6){
